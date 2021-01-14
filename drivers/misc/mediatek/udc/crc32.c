e448fc9c94b24180b305e72d6f9f7a103dc0dc8d
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-01-14 17:26:00 +0530

    drivers: mtk-udc: Drop crc32 def
    * Already defined in kernel
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- drivers/misc/mediatek/udc/crc32.c
+++ drivers/misc/mediatek/udc/crc32.c
@@ -205,39 +205,6 @@ const z_crc_t FAR * ZEXPORT get_crc_table()
 #define DO8 DO1; DO1; DO1; DO1; DO1; DO1; DO1; DO1
 
 /* ========================================================================= */
-unsigned long ZEXPORT crc32(crc, buf, len)
-    unsigned long crc;
-    const unsigned char FAR *buf;
-    uInt len;
-{
-    if (buf == Z_NULL) return 0UL;
-
-#ifdef DYNAMIC_CRC_TABLE
-    if (crc_table_empty)
-        make_crc_table();
-#endif /* DYNAMIC_CRC_TABLE */
-
-#ifdef BYFOUR
-    if (sizeof(void *) == sizeof(ptrdiff_t)) {
-        z_crc_t endian;
-
-        endian = 1;
-        if (*((unsigned char *)(&endian)))
-            return crc32_little(crc, buf, len);
-        else
-            return crc32_big(crc, buf, len);
-    }
-#endif /* BYFOUR */
-    crc = crc ^ 0xffffffffUL;
-    while (len >= 8) {
-        DO8;
-        len -= 8;
-    }
-    if (len) do {
-        DO1;
-    } while (--len);
-    return crc ^ 0xffffffffUL;
-}
 
 #ifdef BYFOUR
 
